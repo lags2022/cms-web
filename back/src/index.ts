0021e03b4cc5ec7151a87f344152e10ac4c03417
lags2022 <lguzman.58erb@outlook.com>
2023-09-05 18:58:56 -0500

    first commit

---
+++ back/src/index.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+import server from './server'
+import { findAvailablePort } from './utils/findAvailablePort'
+
+const desiredPort = process.env.PORT ?? 3001
+// para asignar un puerto especifico en la consola con variable de entorno: PORT=3002 node src/index.js
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+findAvailablePort(desiredPort).then(port =>
+  server.listen(port, () => {
+    console.log(`server listening on port http://localhost:${port}`)
+  })
+)
---
+++ back/src/utils/findAvailablePort.ts
@@ -0,0 +1,28 @@
+import net from 'node:net'
+
+// aqui colocamos esto para que el linter no corrija algunas lineas
+// eslint-disable-next-line
+export const findAvailablePort = (desiredPort: number | string) => {
+  return new Promise((resolve, reject) => {
+    const server = net.createServer()
+
+    server.listen(desiredPort, () => {
+      const { port } = server.address() as net.AddressInfo
+      server.close(() => {
+        resolve(port)
+      })
+    })
+
+    // no olvidar que node siempre escucha los eventos
+    // La expresión server.listen(0) en Node.js hace que el servidor escuche en un puerto disponible de forma automática. Cuando se pasa 0 como argumento para listen(), Node.js seleccionará automáticamente un puerto disponible y asignará ese puerto al servidor.
+    server.on('error', (err: NodeJS.ErrnoException) => {
+      // tbm se puede usar err: Any
+      if (err.code === 'EADDRINUSE') {
+        // eslint-disable-next-line
+        findAvailablePort(0).then(port => resolve(port))
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
